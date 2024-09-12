3849a222ba5dd448a6efb60f536d0d7218796c0d
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2024-09-12 18:06:32 +0300

    done

--- app/src/main/java/com/example/in00ct07_3007_week3_homework/MainActivity.kt
+++ app/src/main/java/com/example/in00ct07_3007_week3_homework/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -20,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,30 +51,46 @@ fun LoginView() {
 }
 @Composable
 fun TextFieldWithIcons() {
-    val textState = remember { mutableStateOf(TextFieldValue("")) }
+    val nameState = remember { mutableStateOf(TextFieldValue("")) }
+    val passwordState = remember { mutableStateOf(TextFieldValue("")) }
+
     Column(
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier.padding(top=16.dp)
     ) {
+        Text(
+            text = "Login",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(bottom = 16.dp, start = 50.dp)
+        )
+        Row(modifier = Modifier.padding(bottom = 16.dp, start = 50.dp)){
     OutlinedTextField(
-        value = textState.value,
-        leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "emailIcon") },
+        value = nameState.value,
+        trailingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "emailIcon") },
         onValueChange = { newValue ->
-            textState.value = newValue
+            nameState.value = newValue
         },
         singleLine = true,
         label = { Text(text = "Email address") },
         placeholder = { Text(text = "Enter your e-mail") },
         modifier = Modifier.padding(bottom = 16.dp),
-    )
+    )}
+        Row(modifier = Modifier.padding(bottom = 16.dp, start = 50.dp)){
     OutlinedTextField(
-        value = textState.value,
-        leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "lockIcon") },
+        value = passwordState.value,
+        trailingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "lockIcon") },
         onValueChange = { newValue ->
-            textState.value = newValue
+            passwordState.value = newValue
         },
         singleLine = true,
         label = { Text(text = "Password") },
+
         placeholder = { Text(text = "Enter your password") },
-    )
+        visualTransformation = PasswordVisualTransformation(),
+    )}
+        Row(modifier = Modifier.padding(bottom = 16.dp, start = 150.dp)){
+            Button(onClick = { /*TODO*/ }) {
+                Text("Login")
+            }
+        }
 }
 }
